d5d1870b5101a8ec2862c47f98459ebf962086c3
prashantsingh2408 <prashantsingh2408@gmail.com>
2025-02-22 20:44:56 +0530

    change backend url to server url

--- src/hooks/useChat.jsx
+++ src/hooks/useChat.jsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useEffect, useState } from "react";
 
-const backendUrl = import.meta.env.VITE_API_URL || "http://localhost:3000";
+const backendUrl = import.meta.env.VITE_API_URL || "https://mentor-ai-avatar-back-end.vercel.app";
 
 const ChatContext = createContext();
 
